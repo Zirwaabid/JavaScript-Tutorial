6ca9d91563b8f037ff4bfdcb1c5031de2850806d
Zirwa Abid <zirwaabid112@gmail.com>
2024-07-24 00:41:42 -0700

    practice

--- Lecture 3/Loops/script.js
+++ Lecture 3/Loops/script.js
@@ -1,43 +1,43 @@
 // for loop 
-// for (let i = 0; i < 5; i++) {
-//     console.log("Zirwa Abid")
-// }
-// for (let count = 0; count <= 3; count++) {
-//     console.log("orange")
-// }
-// console.log("loop has ended")
-// let sum = 0;
-// let num=5;
-// for (let i = 0; i <= num ; i++) {
-//     sum = sum + i; // sum=0+0=0,sum=0+1=1,sum=1+2=3,sum=3+3=6,sum=6+4=10,sum=10+5=15,
-// }
-// console.log("sum =",sum)
-// for (let i = 0; i <=8 ; i++) {
-//    console.log("i=",i)
+for (let i = 0; i < 5; i++) {
+    console.log("Zirwa Abid")
+}
+for (let count = 0; count <= 3; count++) {
+    console.log("orange")
+}
+console.log("loop has ended")
+let sum = 0;
+let num=5;
+for (let i = 0; i <= num ; i++) {
+    sum = sum + i; // sum=0+0=0,sum=0+1=1,sum=1+2=3,sum=3+3=6,sum=6+4=10,sum=10+5=15,
+}
+console.log("sum =",sum)
+for (let i = 0; i <=8 ; i++) {
+   console.log("i=",i)
     
-// }
+}
 
 // while loop 
-// let i=1;
-// while (i<=5) {
-//     console.log("i=",i)
-//     i++
-// }
+let i=1;
+while (i<=5) {
+    console.log("i=",i)
+    i++
+}
 
 // do while loop (condition is check at last)
-// let y=10; 
-// do {console.log("Azra Abid") //console.log("y=",y)console.log("Azra Abid")
-//     y++
-// } while (y<=14);
+let y=10; 
+do {console.log("Azra Abid") //console.log("y=",y)console.log("Azra Abid")
+    y++
+} while (y<=14);
 
 // for of loop (to print alphabets of anything separately)
-// let str="Abid&Azra";
-// let size=0;
-// for (const i of str) { // iterator means character
-//     console.log("i=",i)
-//     size++
-// }
-// console.log("size of str is:", size)
+let str="Abid&Azra";
+let size=0;
+for (const i of str) { // iterator means character
+    console.log("i=",i)
+    size++
+}
+console.log("size of str is:", size)
 
 // for in loop 
 let obj={
---
+++ Lecture 3/practice/practice.js
@@ -0,0 +1,13 @@
+// print even numbers from 1-100 
+// for (let num = 0; num <= 100; num++) {
+//     if(num%2==0){
+//         console.log(num)
+//     }
+// }
+// set a gamenumber and ask a user to write number until they reach correct number 
+let gamenum=19;
+let usernum=prompt("Guess the nuumber:");
+while (usernum !== gamenum) {
+    
+}
+console.log("Congratulations, you entered the right number")
