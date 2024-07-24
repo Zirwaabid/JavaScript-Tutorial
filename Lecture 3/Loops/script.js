353758de5a16d228ec72e01b6179a05c38850161
Zirwa Abid <zirwaabid112@gmail.com>
2024-07-23 23:48:52 -0700

    Loops

--- Lecture 3/Loops/script.js
+++ Lecture 3/Loops/script.js
@@ -6,13 +6,46 @@
 //     console.log("orange")
 // }
 // console.log("loop has ended")
-let sum = 0;
-let num=5;
-for (let i = 0; i <= num ; i++) {
-    sum = sum + i; // sum=0+0=0,sum=0+1=1,sum=1+2=3,sum=3+3=6,sum=6+4=10,sum=10+5=15,
-}
-console.log("sum =",sum)
-for (let i = 0; i <=8 ; i++) {
-   console.log("i=",i)
+// let sum = 0;
+// let num=5;
+// for (let i = 0; i <= num ; i++) {
+//     sum = sum + i; // sum=0+0=0,sum=0+1=1,sum=1+2=3,sum=3+3=6,sum=6+4=10,sum=10+5=15,
+// }
+// console.log("sum =",sum)
+// for (let i = 0; i <=8 ; i++) {
+//    console.log("i=",i)
     
+// }
+
+// while loop 
+// let i=1;
+// while (i<=5) {
+//     console.log("i=",i)
+//     i++
+// }
+
+// do while loop (condition is check at last)
+// let y=10; 
+// do {console.log("Azra Abid") //console.log("y=",y)console.log("Azra Abid")
+//     y++
+// } while (y<=14);
+
+// for of loop (to print alphabets of anything separately)
+// let str="Abid&Azra";
+// let size=0;
+// for (const i of str) { // iterator means character
+//     console.log("i=",i)
+//     size++
+// }
+// console.log("size of str is:", size)
+
+// for in loop 
+let obj={
+    name:"Azra Abid",
+    Age:50,
+    Profession:"House-wife",
+}
+for (let key in obj) {
+        console.log(key,obj[key])
+        
 }
