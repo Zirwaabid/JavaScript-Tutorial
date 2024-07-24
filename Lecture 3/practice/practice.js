e51c4f9bc520ec775bd54a073471ef973603813c
Zirwa Abid <zirwaabid112@gmail.com>
2024-07-24 01:08:46 -0700

    practice

--- Lecture 3/practice/practice.js
+++ Lecture 3/practice/practice.js
@@ -7,7 +7,7 @@
 // set a gamenumber and ask a user to write number until they reach correct number 
 let gamenum=19;
 let usernum=prompt("Guess the nuumber:");
-while (usernum !== gamenum) {
-    
+while (usernum != gamenum) {
+    usernum=prompt("You entered wrong number. Guess again : ")
 }
 console.log("Congratulations, you entered the right number")
