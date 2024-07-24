ed9e40d5c830f35b94cca9220224a273ce1d7313
Zirwa Abid <zirwaabid112@gmail.com>
2024-07-23 22:46:37 -0700

    lecture 3

---
+++ Lecture 3/Loops/script.js
@@ -0,0 +1,18 @@
+// for loop 
+// for (let i = 0; i < 5; i++) {
+//     console.log("Zirwa Abid")
+// }
+// for (let count = 0; count <= 3; count++) {
+//     console.log("orange")
+// }
+// console.log("loop has ended")
+let sum = 0;
+let num=5;
+for (let i = 0; i <= num ; i++) {
+    sum = sum + i; // sum=0+0=0,sum=0+1=1,sum=1+2=3,sum=3+3=6,sum=6+4=10,sum=10+5=15,
+}
+console.log("sum =",sum)
+for (let i = 0; i <=8 ; i++) {
+   console.log("i=",i)
+    
+}
