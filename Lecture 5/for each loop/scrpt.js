3370788beac0ac8c34b0689e362d147d7d866c6e
Zirwa Abid <zirwaabid112@gmail.com>
2024-08-01 00:03:31 -0700

    new

--- Lecture 5/for each loop/scrpt.js
+++ Lecture 5/for each loop/scrpt.js
@@ -10,9 +10,38 @@ array.forEach((val, idx, array) => {
 });
 // we can print parameters in such as value,index(position at each value),array itself as given upper
 
-// map same as for each loop but it return new array if required 
-let colours=["red","orange","purple"]
-let newarr=colours.map((value) => {
-return value
+// map method (same as for each loop but it return new array if required )
+let cat = [1, 2, 3, 4];
+const newcat = cat.map((value)=>{
+    return value;
 })
-console.log(newarr);
+console.log(newcat);
+
+//filter method (give new array but filter different things from aray)
+// filter even numbers from array 
+let number1 = [1, 2, 3, 4, 5, 6]
+let evennumbers = number1.filter((val) => {
+    return val % 2 === 0;
+})
+console.log(evennumbers);
+
+// filter values greater than 3 
+let newnum = [1, 2, 3, 4, 5, 6];
+let greaterval = newnum.filter((val) => {
+    return val > 3;
+})
+console.log(greaterval);
+
+//reduce method (this method is used if we have various input but we want single output eg)
+// calculate sum 
+let value = [1, 2, 3, 4, 5];
+const sum = value.reduce((result, current) => { //result=>previousvalue/accumulator
+    return result + current
+})
+console.log(sum);
+// find greater value through reduce 
+let n = [3, 4, 5, 6];
+const gn = n.reduce((pre, curr) => {
+    return pre > curr ? pre : curr;
+})
+console.log(gn);
--- Lecture 5/practice/practice.js
+++ Lecture 5/practice/practice.js
@@ -1,28 +1,28 @@
 // print the number of vowels 
-// function countvowels(str) {
-//   let count = 0;
-//   for (const char of str) {
-//     if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
-//       count++
-//     }
-//   }
-//   return count;
-// }
-// let result = countvowels("zirwa");
-// console.log(result);
+function countvowels(str) {
+  let count = 0;
+  for (const char of str) {
+    if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+      count++
+    }
+  }
+  return count;
+}
+let result = countvowels("zirwa");
+console.log(result);
 
 // print same task with arrow function 
-// const countvow = (str) => {
-//   count=0;
-//   for (const c of str) {
-//     if (c === "a" || c === "e" || c === "i" || c === "o" || c === "u") {
-//      count++
-//     }
-//   }
-//   return count;
-// }
-// let vowels=countvow("aeiou");
-// console.log(`the total number of vowels in word is ${vowels}`);
+const countvow = (str) => {
+  count=0;
+  for (const c of str) {
+    if (c === "a" || c === "e" || c === "i" || c === "o" || c === "u") {
+     count++
+    }
+  }
+  return count;
+}
+let vowels=countvow("aeiou");
+console.log(`the total number of vowels in word is ${vowels}`);
 
 // print the square of each value using for each loop
 let array=[1,3,4,6];
@@ -30,3 +30,29 @@ array.forEach( array=> {
  console.log(array*array);
 });
 
+// filter 90+ marks 
+let marks = [49, 50, 10, 91, 80, 95, 99, 100, 98];
+const newmarks = marks.filter((value) => {
+  return value>90;
+})
+console.log(`marks that greater than 90 is ${newmarks}`);
+
+// take a number n as input from user and create array of these number
+let number = prompt("Enter any number:");
+let arr = [];
+for (let i = 1; i <= number; i++) {
+    arr[i - 1] = i;
+
+}
+console.log(`all numbers=${arr}`);
+
+// use reduce method to sum the numbers in array
+const sum = arr.reduce((res, current) => {
+    return res + current;
+})
+console.log(`sum=${sum}`);
+// use reduce method to find factorial of numbers in array 
+const factorial = arr.reduce ((result, current) => {
+    return result * current;
+})
+console.log(`factorial=${factorial}`);
