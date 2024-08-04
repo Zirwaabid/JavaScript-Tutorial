32243fd5617872e6a65d12aa42d88223f0e8ba09
Zirwa Abid <zirwaabid112@gmail.com>
2024-08-04 06:17:50 -0700

    new commit

--- lecture 6/Practice/script.js
+++ lecture 6/Practice/script.js
@@ -1,8 +1,22 @@
-// homework:
-// from mdn 
-// read about parent, children and siblings
-// read about first ChannelSplitterNode,last child 
-
 // add new string to h2
 let heading = document.querySelector("h2");
 heading.innerText = heading.innerText + " from Apna College students";
+
+// add something new in each div 
+// let dives=document.querySelectorAll("div");
+// dives[0].innerText="new value 1";
+// dives[1].innerText="new value 2";
+// dives[2].innerText="new value 3;
+
+// perform same task through loop (professional way)
+let dives=document.querySelectorAll("div");
+let index=1;
+for (const div of dives) {
+    div.innerText=`new value ${index}`
+    index++
+}
+
+
+
+
+
