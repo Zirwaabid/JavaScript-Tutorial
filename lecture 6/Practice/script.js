1f288ababe803211cdd56d44f6f1eb371c131ec8
Zirwa Abid <zirwaabid112@gmail.com>
2024-08-04 06:19:48 -0700

    new commit

--- lecture 6/Practice/script.js
+++ lecture 6/Practice/script.js
@@ -3,11 +3,6 @@ let heading = document.querySelector("h2");
 heading.innerText = heading.innerText + " from Apna College students";
 
 // add something new in each div 
-// let dives=document.querySelectorAll("div");
-// dives[0].innerText="new value 1";
-// dives[1].innerText="new value 2";
-// dives[2].innerText="new value 3;
-
 // perform same task through loop (professional way)
 let dives=document.querySelectorAll("div");
 let index=1;
@@ -16,6 +11,10 @@ for (const div of dives) {
     index++
 }
 
+// let dives=document.querySelectorAll("div");
+// dives[0].innerText="new value 1";
+// dives[1].innerText="new value 2";
+// dives[2].innerText="new value 3";
 
 
 
