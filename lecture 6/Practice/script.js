844da11cd1a86c116caacac8c9b7dace3ae22268
Zirwa Abid <zirwaabid112@gmail.com>
2024-08-04 05:48:41 -0700

    new cmmit

--- lecture 6/Practice/script.js
+++ lecture 6/Practice/script.js
@@ -1,4 +1,8 @@
 // homework:
-                // from mdn 
+// from mdn 
 // read about parent, children and siblings
 // read about first ChannelSplitterNode,last child 
+
+// add new string to h2
+let heading = document.querySelector("h2");
+heading.innerText = heading.innerText + " from Apna College students";
