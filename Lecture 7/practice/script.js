03669e37cfb31d7e0eb5b1021d89220d539ba46f
Zirwa Abid <zirwaabid112@gmail.com>
2024-08-05 12:50:37 -0700

    new commit

--- Lecture 7/practice/script.js
+++ Lecture 7/practice/script.js
@@ -1,2 +1,12 @@
+// 1-add button and give styling through js 
 let button=document.createElement("button");
 button.innerText="Click me!";
+button.style.backgroundColor="red";
+button.style.color="white";
+document.querySelector("body").append(button);
+
+// 2-give new class to para in css and link it to the para throug js using classList
+let para=document.querySelector("p");
+para.classList.add("para2");
+
+
