87fd06d67b11d341a4cbe95bcf187f96bb7c5512
Zirwa Abid <zirwaabid112@gmail.com>
2024-08-04 23:05:21 -0700

    new commit

--- Lecture 7/DOM(part3)/script.js
+++ Lecture 7/DOM(part3)/script.js
@@ -1,2 +1,30 @@
 let div=document.querySelector("div");
 console.log(div);
+let p=document.querySelector("p")
+console.log(p);
+
+// how to access attributes in javascript 
+
+// attribute present inside div 
+let id=div.getAttribute("id");
+console.log(id);
+
+let name=div.getAttribute("name");
+console.log(name);
+
+// attribute present inside p 
+console.log(p.getAttribute("class"));
+
+// how to change attributes name through javascript 
+console.log(p.setAttribute("class","newClass"));
+
+//how to access style in js
+console.log(div.style);
+div.style.backgroundColor="cyan";
+div.style.fontSize="23px";
+div.innerText="Pakistan";
+
+
+
+
+
