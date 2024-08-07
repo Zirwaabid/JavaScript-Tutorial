4c2b3870109bb6fb20719f6c11e7320d4caa07f3
Zirwa Abid <zirwaabid112@gmail.com>
2024-08-07 11:55:47 -0700

    events in js

--- Lecture 8/Events in JavaScript/script.js
+++ Lecture 8/Events in JavaScript/script.js
@@ -6,8 +6,18 @@ btn1.onclick = () => { //1click
     console.log(a); //26
 }
 
-let box=document.querySelector("div");
-box.onmouseover=()=>{
+let box = document.querySelector("div");
+box.onmouseover = () => {
     console.log("you are inside div");
     alert("hi!");
+}
+
+//access all the info about events used in element eg button used below:
+btn2 = document.querySelector("#btn2")
+btn2.ondblclick = (events) => {
+    console.log(events);
+    // below all info is available in events but we also print it separately 
+    console.log(events.type); //type=dblclick
+    console.log(events.target);
+    console.log(events.clientX, events.clientY);
 }
