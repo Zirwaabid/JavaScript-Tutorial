4d798e1eca2dbcb5a9d9002ff3c88fd619c302be
Zirwa Abid <zirwaabid112@gmail.com>
2024-08-07 20:12:01 -0700

    events in js

--- Lecture 8/Practice/script.js
+++ Lecture 8/Practice/script.js
@@ -1,13 +1,31 @@
-let modebtn=document.querySelector(".mode");
-let currMode="light";
-modebtn.addEventListener("click",()=>{
-   if (currMode==="light") {
-    currMode="dark";
-    document.querySelector("body").style.backgroundColor="black";
-   }
-    else{
-    currMode="light";
-    document.querySelector("body").style.backgroundColor="white";
-   }
-   console.log(currMode);
+let modebtn = document.querySelector(".mode");
+let body = document.querySelector("body");
+// // create light and dark mode through js 
+let currMode1 = "light";
+modebtn.addEventListener("click", () => {
+    if (currMode1 === "light") {
+        currMode1 = "dark";
+        body.style.backgroundColor = "black";
+    }
+    else {
+        currMode1 = "light";
+        body.style.backgroundColor = "white";
+    }
+    console.log(currMode1);
+});
+
+//create light and dark mode through css (create classes and add in js)nad add it in js
+let currMode = "light";
+
+modebtn.addEventListener("click", () => {
+    if (currMode === "light") {
+        currMode = "dark";
+        document.querySelector("body").classList.remove("light");
+        body.classList.add("dark");
+    } else {
+        currMode = "light";
+        document.querySelector("body").classList.remove("dark");
+        body.classList.add("light");
+    }
+    console.log(currMode);
 });
