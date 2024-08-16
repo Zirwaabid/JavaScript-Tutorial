8c2ffed083b5a44858ac3beb19af26dc8d5cf684
Zirwa Abid <zirwaabid112@gmail.com>
2024-08-16 11:49:02 -0700

    Lecture 10

--- Lecture 10/script.js
+++ Lecture 10/script.js
@@ -1,9 +1,15 @@
 let userScore=0;
 let compScore=0;
-const choices=document.querySelectorAll(".choices");
+const choices=document.querySelectorAll(".choice");
+
+const playGame=()=>{
+
+}
 
 choices.forEach((choice)=>{
     choice.addEventListener("click",()=>{
-
+    const userChoice=choice.getAttribute("id");
+    console.log(`${userChoice} was clicked`);
+     playGame();
     })
 });
