bc024f636ee1cf276a252cee96bf7aa7d63ecc89
Zirwa Abid <zirwaabid112@gmail.com>
2024-08-16 12:39:31 -0700

    Lecture 10

--- Lecture 10/script.js
+++ Lecture 10/script.js
@@ -1,15 +1,46 @@
-let userScore=0;
-let compScore=0;
-const choices=document.querySelectorAll(".choice");
+let userScore = 0;
+let compScore = 0;
+const choices = document.querySelectorAll(".choice");
 
-const playGame=()=>{
+const drawGame = () => {
+    console.log("game was draw");
+};
 
+const genCompChoice = () => {
+    const options = ["rock", "paper", "scissors"];
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
 }
 
-choices.forEach((choice)=>{
-    choice.addEventListener("click",()=>{
-    const userChoice=choice.getAttribute("id");
-    console.log(`${userChoice} was clicked`);
-     playGame();
+const playGame = (userChoice) => {
+    console.log(`userChoice=${userChoice}`);
+    //generate compChoice
+    const compChoice = genCompChoice();
+    console.log(`compChoice=${compChoice}`);
+    if (userChoice === compChoice) {
+        drawGame();
+    }
+    else {
+        let userWin = true;
+        if (userChoice === "rock") {
+            // compchoice=paper,scissors
+           userWin= compChoice === "paper" ? false : ture;
+        }
+      else if(userChoice === "paper"){
+          // compchoice=rock,scissors
+          userWin= compChoice === "scissors" ? false : true;
+      }
+      else{
+         // compchoice=paper,rock
+         userWin= compChoice === "rock" ? false : true;
+      }
+      showWinner();
+    }
+};
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
     })
 });
