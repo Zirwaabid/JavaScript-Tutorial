6987afaddeade9ea9321a740901bcd2b27f88924
Zirwa Abid <zirwaabid112@gmail.com>
2024-08-16 18:42:02 -0700

    lecture 10

--- Lecture 10/script.js
+++ Lecture 10/script.js
@@ -1,9 +1,26 @@
 let userScore = 0;
 let compScore = 0;
 const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
 
 const drawGame = () => {
-    console.log("game was draw");
+    msg.innerText = "Game was Draw Play Again!";
+    msg.style.backgroundColor = "#081b31";
+};
+
+let showWinner = (userWin, userChoice, compChoice) => {
+    if (userWin) {
+        userScore++
+        msg.innerText = `You Win! your ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "green";
+    }
+    else {
+        msg.innerText = "You lose!";
+        msg.style.backgroundColor = "red";
+        msg.innerText = `You lost! ${compChoice} beats your ${userChoice}`;
+    };
 };
 
 const genCompChoice = () => {
@@ -24,17 +41,17 @@ const playGame = (userChoice) => {
         let userWin = true;
         if (userChoice === "rock") {
             // compchoice=paper,scissors
-           userWin= compChoice === "paper" ? false : ture;
+            userWin = compChoice === "paper" ? false : ture;
         }
-      else if(userChoice === "paper"){
-          // compchoice=rock,scissors
-          userWin= compChoice === "scissors" ? false : true;
-      }
-      else{
-         // compchoice=paper,rock
-         userWin= compChoice === "rock" ? false : true;
-      }
-      showWinner(userWin);
+        else if (userChoice === "paper") {
+            // compchoice=rock,scissors
+            userWin = compChoice === "scissors" ? false : true;
+        }
+        else {
+            // compchoice=paper,rock
+            userWin = compChoice === "rock" ? false : true;
+        }
+        showWinner(userWin, userChoice, compChoice);
     }
 };
 
