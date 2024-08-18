9307717298722bdd13b4d62a3558481700e92273
Zirwa Abid <zirwaabid112@gmail.com>
2024-08-18 12:36:40 -0700

    lecture 11

--- Lecture 11/Classes/script.js
+++ Lecture 11/Classes/script.js
@@ -1,11 +1,66 @@
 class ToyotaCar {
-   start(){
-    console.log("Start");
+   constructor(brand, mileage) {
+      console.log("Hello buyers")
+      this.brand = brand;
+      this.mileage = mileage;
+
    }
-   stop(){
-    console.log("stop");
+   start() {
+      console.log("Start");
    }
+   stop() {
+      console.log("stop");
+   }
+
+
 }
 // we can create many objects through this template (class) 
-let Fortuner=new ToyotaCar();
-let lexus=new ToyotaCar();
+let Fortuner = new ToyotaCar("fortuner", 14);
+console.log(Fortuner);
+let lexus = new ToyotaCar("lexus", 12);
+console.log(lexus);
+
+// how to inherit parent class in child class 
+// eg 
+class parent {
+   
+   hello() {
+      console.log("hello");
+   }
+};
+
+class child extends parent { };
+let obj = new child();
+
+//another example
+
+class person {
+   constructor(){
+      this.species="homo sapiens";
+   }
+   eat() {
+      console.log("eat");
+   }
+   sleep() {
+      console.log("sleep");
+   }
+};
+
+class engineer extends person {
+   work() {
+      console.log("solve problms, bulid something")
+   }
+};
+let Zirwa = new engineer();
+
+class doctor extends person {
+   work() {
+      console.log("treat patients");
+   }
+};
+
+ let e1=new engineer();
+ let p1=new person();
+ console.log(e1);
+ console.log(p1);
+
