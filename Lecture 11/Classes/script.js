48302224c7826a594385414a41eac44622fed682
Zirwa Abid <zirwaabid112@gmail.com>
2024-08-17 18:14:11 -0700

    Lecture 11

---
+++ Lecture 11/Classes/script.js
@@ -0,0 +1,11 @@
+class ToyotaCar {
+   start(){
+    console.log("Start");
+   }
+   stop(){
+    console.log("stop");
+   }
+}
+// we can create many objects through this template (class) 
+let Fortuner=new ToyotaCar();
+let lexus=new ToyotaCar();
