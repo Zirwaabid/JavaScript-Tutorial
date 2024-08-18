e498c239f7ccb27d15ed6be7b239789e79e75602
Zirwa Abid <zirwaabid112@gmail.com>
2024-08-17 17:35:37 -0700

    lecture 11

---
+++ Lecture 11/script.js
@@ -0,0 +1,13 @@
+const Student = {
+    fullName: "Zirwa",
+    Marks: 98.9,
+    printMarks: function () {
+        console.log("marks=", this.Marks); //this.Marks means student.Marks(repsent object)
+    },
+};
+
+const employee={
+    calctax(){
+        console.log("tax rate is 10%");
+    },
+};
