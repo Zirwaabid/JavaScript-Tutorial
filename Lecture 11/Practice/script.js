0f785ced9345325ee4e8023f75c8747a217bbea9
Zirwa Abid <zirwaabid112@gmail.com>
2024-08-19 12:13:13 -0700

    Lecture 11

---
+++ Lecture 11/Practice/script.js
@@ -0,0 +1,23 @@
+let data="Website Data";
+class user{
+    constructor(name,email){
+        this.name=name;
+        this.email=email;
+    }
+    viewData(){
+        console.log("data =",data);
+    }
+};
+class admin extends user{
+    constructor(name,email){
+        super(name,email)
+    }
+   editData(){
+    data="secret information"
+   }
+}
+
+let student1=new user("Zirwa","Zirwa@gmail.com");
+let student2=new user("Anas","Anas@gmail.com");
+let teacher=new user("Azra","Azra@gmail.com")
+let admin1=new admin("Abid","Abid@gmail.com");
--- Lecture 11/Prototype/script.js
+++ Lecture 11/Prototype/script.js
@@ -30,3 +30,20 @@ Anas2.__proto__ = employee;
 Anas3.__proto__ = employee;
 
 //if there is same method used in object or prototype of another object than method f=of their own objet win
+
+//error handling 
+// if we have chance of error in some line then we use this keywords for this line 
+let a = 6;
+let b = 3;
+console.log(a);
+console.log(b);
+console.log(a + b);
+console.log(a + b);
+
+try {
+    console.log(a + c); //error
+} catch (err) {
+    console.log(err);
+}
+
+console.log(a + b);
