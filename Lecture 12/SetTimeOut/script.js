57b38ebe3042f3f9409e6210d230270fa80f3903
Zirwa Abid <zirwaabid112@gmail.com>
2024-08-20 17:53:06 -0700

    Lecture 12

---
+++ Lecture 12/SetTimeOut/script.js
@@ -0,0 +1,7 @@
+console.log("one");
+console.log("two");
+setTimeout(()=>{
+},2000);
+// console.log("hello") //asynchronous (we use settimeout to execute code without wait this code)
+console.log("three");
+console.log("four");
