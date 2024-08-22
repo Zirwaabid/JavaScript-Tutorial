54c9a858ee721eb7dc6b387cd54b11edfd5a2ea6
Zirwa Abid <zirwaabid112@gmail.com>
2024-08-22 11:12:42 -0700

    lecture 13

--- Lecture 12/comparison/script.js
+++ Lecture 12/comparison/script.js
@@ -2,69 +2,69 @@
 
 //callback hell
 
-// function getData(DataId, getNextdata) {
-//     setTimeout(() => {
-//         console.log("data =", DataId)
-//         if (getNextdata) {
-//             getNextdata();
-//         }
-//     }, 2000);
-// };
+function getData(DataId, getNextdata) {
+    setTimeout(() => {
+        console.log("data =", DataId)
+        if (getNextdata) {
+            getNextdata();
+        }
+    }, 2000);
+};
 
 
-// getData(1, () => {
-//     console.log("getting data of 2......")
-//     getData(2,()=>{
-//         console.log("getting data of 3......")
-//         getData(3,()=>{
-//             console.log("getting data of 4......")
-//             getData(4)
-//         });
-//     });
-// });
+getData(1, () => {
+    console.log("getting data of 2......")
+    getData(2,()=>{
+        console.log("getting data of 3......")
+        getData(3,()=>{
+            console.log("getting data of 4......")
+            getData(4)
+        });
+    });
+});
 
 //promises chaining
 
-// function getdata(dataId) {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             console.log("data=", dataId)
-//             resolve("success")
-//         }, 2000);
-//     });
-// };
-// console.log("fetching data1.....")
-// getdata(1)
-//     .then((res) => {
-//         console.log("fetching data2.....")
-//         return getdata(2);
-//     })
-//     .then((res) => {
-//         console.log("fetching data3.....")
-//         return getdata(3);
-//     })
-//     .then((res) => {
-//         console.log("fetching data4.....")
-//         return getdata(4);
-//     })
-//     .then((res) => {
-//         console.log(res);
-//     });
+function getdata(dataId) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("data=", dataId)
+            resolve("success")
+        }, 2000);
+    });
+};
+console.log("fetching data1.....")
+getdata(1)
+    .then((res) => {
+        console.log("fetching data2.....")
+        return getdata(2);
+    })
+    .then((res) => {
+        console.log("fetching data3.....")
+        return getdata(3);
+    })
+    .then((res) => {
+        console.log("fetching data4.....")
+        return getdata(4);
+    })
+    .then((res) => {
+        console.log(res);
+    });
 
 
 //simpler way given above of the following solution
-// console.log("fetching data2.....")
-// getdata(2).then((res) => {
-//     console.log(res);
-//     console.log("fetching data3.....");
-//     getdata(3).then((res) => {
-//         console.log(res);
-//         console.log("fetching data4.....");
-//         getdata(4).then((res) => {
-//             console.log(res);
-//         });
-//     });
-// });
+console.log("fetching data2.....")
+getdata(2).then((res) => {
+    console.log(res);
+    console.log("fetching data3.....");
+    getdata(3).then((res) => {
+        console.log(res);
+        console.log("fetching data4.....");
+        getdata(4).then((res) => {
+            console.log(res);
+        });
+    });
+});
 
 //async await
 function getdata(dataId) {
